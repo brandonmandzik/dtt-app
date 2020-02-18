afb3e1d048c0daa5fcb397f8f87c620b343af6dc
brandonmandzik <brandon.m@gmx.de>
2020-02-18 11:52:40 +0100

    added background vid

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,55 @@
 <template>
-  <div id="app" class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5 mt-2">
-      <span class="navbar-brand h1">DTT</span>
-      <div class="navbar-nav navbar-collapse">
-        <a class="nav-item nav-link active ml-5" href="">Home </a>
-        <a class="nav-item nav-link" href="">Random</a>
-        <a class="nav-item nav-link" href="">About Me</a>
-      </div>  
-    </nav>
-    <entry-table :entries="entries"/>
+  <div id="app">
+    <video autoplay muted loop class="position-fixed" id="bgVid">
+      <source src="./assets/vid/backgroundVid.mp4" type="video/mp4" />
+    </video>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5 mt-2">
+        <span class="navbar-brand h1">DTT</span>
+        <div class="navbar-nav navbar-collapse">
+          <a class="nav-item nav-link active ml-5" href>Home</a>
+          <a class="nav-item nav-link" href>Random</a>
+          <a class="nav-item nav-link" href>About Me</a>
+        </div>
+      </nav>
+      <entry-table :entries="entries" />
+    </div>
   </div>
 </template>
 
 <script>
-import EntryTable from "./components/EntryTable.vue"
+import EntryTable from "./components/EntryTable.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     EntryTable
   },
   mounted() {
-   this.getAPIs() 
+    this.getAPIs();
   },
   data() {
-    return{
+    return {
       entries: []
-    }
+    };
   },
-  methods:{
-    async getAPIs(){
+  methods: {
+    async getAPIs() {
       for (let i = 0; i < 10; i++) {
-        const response = await fetch("https://api.publicapis.org/random")
-        const data = await response.json()
-        const id = i
-        const newEntry = {...data["entries"][0],id}
-        this.entries = [...this.entries, newEntry]
+        const response = await fetch("https://api.publicapis.org/random");
+        const data = await response.json();
+        const id = i;
+        const newEntry = { ...data["entries"][0], id };
+        this.entries = [...this.entries, newEntry];
       }
-    },
+    }
   }
-}
+};
 </script>
 
 <style>
+#bgVid{
+  min-width: 100%;
+  min-height: 100%;
+}
 </style>
