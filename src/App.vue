7dbc3817639312d48ff57a4866ed1b05b0a03b39
brandonmandzik <brandon.m@gmx.de>
2020-02-18 01:43:02 +0100

    basic home + navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5 mt-2">
+      <span class="navbar-brand h1">DTT</span>
+      <div class="navbar-nav navbar-collapse">
+        <a class="nav-item nav-link active ml-5" href="">Home </a>
+        <a class="nav-item nav-link" href="">Random</a>
+        <a class="nav-item nav-link" href="">About Me</a>
+      </div>  
+    </nav>
+    <entry-table :entries="entries"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EntryTable from "./components/EntryTable.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EntryTable
+  },
+  mounted() {
+   this.getAPIs() 
+  },
+  data() {
+    return{
+      entries: []
+    }
+  },
+  methods:{
+    async getAPIs(){
+      for (let i = 0; i < 10; i++) {
+        const response = await fetch("https://api.publicapis.org/random")
+        const data = await response.json()
+        const id = i
+        const newEntry = {...data["entries"][0],id}
+        this.entries = [...this.entries, newEntry]
+      }
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/EntryTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="entry-table">
+       <div v-for="entry in entries" :key="entry.id" class="row justify-content-center my-5">
+            <div class="card w-50">
+                <div class="card-body">
+                    <h5 class="card-title">{{entry.API}}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{{entry.Category}}</h6>
+                    <p class="card-text">{{entry.Description}}</p>
+                </div>
+            </div>
+       </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "entry-table",
+    props: {
+        entries: Array,
+    },
+    data() {
+        return{
+
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
